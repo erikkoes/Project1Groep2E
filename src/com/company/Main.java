425f338d89a3ead6df520a43bc6084b0096cf1a1
stijndekrieger <stijndekrieger@gmail.com>
2021-03-10 11:29:17 +0100

    test print 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("test");
+        System.out.println("test2");
 	// write your code here
     }
     //testestest
