cdfd60e690687de6b0a060d784df12aa290c231b
stijndekrieger <stijndekrieger@gmail.com>
2021-03-10 11:33:37 +0100

    remove test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("test");
-        System.out.println("test2");
-	// write your code here
-    testclass e = new testclass(3);
+
+
     }
     //testestest
 }
