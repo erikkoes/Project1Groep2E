9400e0d45e30e3d1345c1c5d05026d91a56d6fa2
erikkoes <19086296@student.hhs.nl>
2021-03-10 11:30:42 +0100

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,15 @@ public class Main {
         System.out.println("test");
         System.out.println("test2");
 	// write your code here
+    testclass e = new testclass(3);
     }
     //testestest
+}
+
+class testclass{
+    public static Integer e;
+    testclass(Integer e){
+
+        e=e;
+    }
 }
