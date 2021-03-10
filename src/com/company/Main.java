7062ef197a658228d6d23771d69dfe1f89395df7
stijndekrieger <stijndekrieger@gmail.com>
2021-03-10 12:00:46 +0100

    Examen getters & setters

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -34,10 +34,22 @@ class Examen {
     private Integer aantalVragen;
     private String soortVragen;
 
-    Examen(String naam, Integer aantalVragen, String soortVragen) {
+    public Examen(String naam, Integer aantalVragen, String soortVragen) {
         this.naam=naam;
         this.aantalVragen=aantalVragen;
         this.soortVragen=soortVragen;
+    }
+
+    public String getNaam() {
+        return this.naam;
+    }
+
+    public Integer getAantalVragen() {
+        return this.aantalVragen;
+    }
+
+    public String getSoortVragen() {
+        return this.soortVragen;
     }
 
     
