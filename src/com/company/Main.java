2ddb1d2cfb623880a511009a2314e28ec96b4925
stijndekrieger <stijndekrieger@gmail.com>
2021-03-11 09:52:55 +0100

    Merge branch 'main' of C:\HHS\OPT\Project1Groep2E with conflicts.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,39 +1,17 @@
 package com.company;
-import java.util.*;
 
 class Main {
 
     public static void main(String[] args) {
-
-    }
-}
-
-class Menu {
-
-    Menu(){
-        System.out.println("Welkom bij het examencommissie programma van projectgroep 2E");
-    }
-
-    public void displayMenu(){
-        System.out.println("1) Lijst met examens");
-        System.out.println("2) Lijst met studenten");
-        System.out.println("3) Nieuwe student inschrijven");
-        System.out.println("4) Student verwijderen");
-        System.out.println("5) Examen afnemen");
-        System.out.println("6) Is student geslaagd voor test?");
-        System.out.println("7) Welke examens heeft student gehaald?");
-        System.out.println("8) Welke student heeft de meeste examens gehaald?");
-        System.out.println("0) Exit");
+        System.out.println("Hello");
 
     }
 }
 
 class Vraag {
-
     private String typeVraag;
     private String vraag;
     private String goedAntwoord;
-    
     Vraag(String typeVraag, String vraag, String goedAntwoord){
        this.typeVraag=typeVraag;
        this.vraag = vraag;
@@ -48,7 +26,6 @@ class Vraag {
    public String getTypeVraag(){
        return this.typeVraag;
    }
-
 }
 
 
