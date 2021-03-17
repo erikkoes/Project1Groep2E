6cb3531b0e00733173d1f571f52bd3841dbed664
stijndekrieger <stijndekrieger@gmail.com>
2021-03-17 10:34:15 +0100

    constructors van vragen subclasses

--- src/com/company/MeerkeuzeVraag.java
+++ src/com/company/MeerkeuzeVraag.java
@@ -1,4 +1,6 @@
 class MeerkeuzeVraag extends Vraag{
-    
 
+    MeerkeuzeVraag(String vraag, String goedAntwoord) {
+        super(vraag, goedAntwoord);
+    }
 }
--- src/com/company/OpenVraag.java
+++ src/com/company/OpenVraag.java
@@ -1,3 +1,6 @@
 class OpenVraag extends Vraag{
-    
+
+    OpenVraag(String vraag, String goedAntwoord) {
+        super(vraag, goedAntwoord);
+    }
 }
