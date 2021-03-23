179547fdb6a53696b3f647ba0d084df579a66069
stijndekrieger <stijndekrieger@gmail.com>
2021-03-23 11:18:49 +0000

    examen & test vraag

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -20,6 +20,17 @@ class Main {
             Student klaas = new Student(58303853,"Klaas");
             Student bertus = new Student (09159839,"Bertus");
 
+            Examen rekenen = new Examen("Rekenexamen");
+
+            Meerkeuzevraag rekenvraag1 = new Meerkeuzevraag("Wat is 1+1?");
+
+            rekenvraag1.addOptie("4",false);
+            rekenvraag1.addOptie("27",false);
+            rekenvraag1.addOptie("2",true);
+
+            rekenvraag1
+
+
 
 
     }
